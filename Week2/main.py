cc156fd16fc39adcba9fa8a4e33165e54e6b4fa3
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-16 20:04:51 -0400

    rest of the night

--- Week2/main.py
+++ Week2/main.py
@@ -13,9 +13,16 @@ print("7//2: ", 7 // 2)
 # to get a remiander, use modulus
 print("7%2: ", 7 % 2)
 
-people_attending_the_party = 13
-average_number_of_pizza_rolls_eaten_per_person = 12
-pizza_rolls_per_bag = 38
+# asking the user to enter a value, then converting to an int
+people_attending_the_party = \
+    int(input("How many people are coming to the party?"))
+average_number_of_pizza_rolls_eaten_per_person = \
+    float(input("How many pizza rolls will a guest eat on average?"))
+pizza_rolls_per_bag = \
+    int(input("How many pizza rolls come in a bag?"))
+cost_of_bag_of_pizza_rolls = \
+    float(input("how much does a bag of pizza rolls cost? ( no $ please )"))
+
 
 # \ tells python to keep reading the next line as if we didn't stop
 total_pizza_rolls = people_attending_the_party * \
@@ -26,7 +33,9 @@ bags_of_pizza_rolls_needed = math.ceil(bags_of_pizza_rolls_needed)
 
 print("You need to buy", bags_of_pizza_rolls_needed,
       "bags of pizza rolls for the party!")
+print("It will cost $" + str(bags_of_pizza_rolls_needed*cost_of_bag_of_pizza_rolls))
 
+# got it here https://stackoverflow.com/questions/3477283/what-is-the-maximum-float-in-python
 print(1,000,000) # this is wrong and ugly
 print(1, 000, 000) # this is wrong but pretty
 print(1_000_000) # this is right and helpful
@@ -46,3 +55,11 @@ students_attending_in_person = 16
 # str converts something to a string
 print(str(students_attending_in_person / students_enrolled * 100) +
       "% of students attend in person")
+
+
+loan_amount = int(input("enter the total loan amount ( no $ please) "))
+interest_rate = float(input("Enter the interest rate like .06: "))
+
+payment = loan_amount * interest_rate / 12
+
+print("interest only monthly payment: $", payment)
